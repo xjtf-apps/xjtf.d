673d2d2e3b9fd7bc8c745cd3f0f60e7e444de036
Filip Vukovinski <filip.vukovinski@net.hr>
2025-01-06 11:08:46 +0100

    changed port

--- ClientApp/src/api/serverApi.ts
+++ ClientApp/src/api/serverApi.ts
@@ -6,27 +6,27 @@ const hostname = 'localhost:7631';
 
 export const serverApi = {
   async getServices(): Promise<WindowsService[]> {
-    return fetch(`https://${hostname}/api/windowsservices`)
+    return fetch(`http://${hostname}/api/windowsservices`)
     .then(response => response.json())
     .then(data => data as WindowsService[])
   },
 
   async getServiceByName(serviceName: string): Promise<WindowsServiceWithUptime | undefined> {
-    return fetch(`https://${hostname}/api/windowsservices/${serviceName}`)
+    return fetch(`http://${hostname}/api/windowsservices/${serviceName}`)
     .then(response => response.json())
     .then(data => data as WindowsServiceWithUptime);
   },
 
   async pinService(serviceName: string): Promise<void> {
-    fetch(`https://${hostname}/api/pinnedservices/pin/${serviceName}`, { method: 'PUT' });
+    fetch(`http://${hostname}/api/pinnedservices/pin/${serviceName}`, { method: 'PUT' });
   },
 
   async unpinService(serviceName: string): Promise<void> {
-    fetch(`https://${hostname}/api/pinnedservices/unpin/${serviceName}`, { method: 'PUT' });
+    fetch(`http://${hostname}/api/pinnedservices/unpin/${serviceName}`, { method: 'PUT' });
   },
 
   async getPinnedServices(): Promise<PinnedServices> {
-    return fetch(`https://${hostname}/api/pinnedservices`)
+    return fetch(`http://${hostname}/api/pinnedservices`)
     .then(response => response.json())
     .then(data => data as PinnedServices);
   }
